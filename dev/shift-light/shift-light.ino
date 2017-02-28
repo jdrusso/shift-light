628cdf6f6f09c694543b864c8f216a751d6437c6
JD Russo <JD Russo>
2017-02-28 16:26:14 -0500

    Rewritten with FastLED instead of Neopixel library

--- dev/shift-light/shift-light.ino
+++ dev/shift-light/shift-light.ino
@@ -3,43 +3,28 @@
 // Ignition frequency is equal to RPM/30
 //initialize libraries
 #include <Bounce2.h>
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
+
 
 
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
 #define PIN            5  //LED Data Pin
 #define BUTTON_PIN     6
-#define NUMPIXELS      12  //number of leds connected
-#define YLWPIXELS       2  //
+#define NUMPIXELS      8  //number of leds connected
 Bounce debouncer = Bounce();
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+CRGB pixels[NUMPIXELS];
 
 
 int i;
 int rpm;
-float ighigh,iglow;
+float ighigh, iglow;
 unsigned long igfreq, igcal1, igcal2;
 int cnt;
-int niday=HIGH;
-
-int _red[] = {100,0,0};
-int _green[] = {0,100,0};
-int _blue[] = {0,0,100};
-int _yellow[] = {100,100,0};
-
-int _dimred[] = {20,0,0};
-int _dimgreen[] = {0,20,0};
-int _dimblue[] = {0,0,20};
-int _dimyellow[] = {20,20,0};
-
-int _off[] = {0,0,0};
-
-int MIN_RPM = 3000;
-int SHIFT_RPM = 6200;
-int MAX_RPM = 6900;
+int niday = HIGH;
 
 int manual_igfreq;
 int brightness_state = 1;
@@ -47,175 +32,151 @@ int brightness_state = 1;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(BUTTON_PIN,INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   debouncer.attach(BUTTON_PIN);
   debouncer.interval(5);
-  pixels.begin(); // This initializes the NeoPixel library.
-  Serial.begin(115200);        // connect to the serial port
+//  Serial.begin(115200);        // connect to the serial port
 
-  Serial.println("Frequency Counter"); //this is jsut for debugging so we can see what freq the controller is reading
+//  Serial.println("Frequency Counter"); //this is jsut for debugging so we can see what freq the controller is reading
 
   manual_igfreq = 100;
 
-  if (YLWPIXELS > NUMPIXELS){
-    Serial.println("Error. Number of yellow pixels set incorrectly");
-    exit(-1);
-  }
+  FastLED.addLeds<NEOPIXEL, PIN>(pixels, NUMPIXELS);
+  FastLED.setBrightness(100);
 }
 
 void loop() {
 
   //DEBUGGING CODE - Manually set RPM using a digit between 0-7
-//  char manual_RPM = Serial.read();
-//  Serial.println("Read");
-//  int manual_igfreq = 100 + 7 * manual_RPM; 
-  if (manual_igfreq <= 250){
+  //  char manual_RPM = Serial.read();
+  //  Serial.println("Read");
+  //  int manual_igfreq = 100 + 7 * manual_RPM;
+  if (manual_igfreq <= 250) {
 
     manual_igfreq++;
     delay(10);
   } else {
     manual_igfreq = 100;
   }
-  
+
   //loop code, runs every cycle
 
   // Update the Bounce instance to see if button was pressed:
-   debouncer.update();
+  debouncer.update();
 
-   // Call code if Bounce fell (transition from HIGH to LOW) :
-   if ( debouncer.fell() ) {
+  // Call code if Bounce fell (transition from HIGH to LOW) :
+  if ( debouncer.fell() ) {
 
-     Serial.println("Button pressed");
-     // Toggle LED state if button was pressed :
-     niday = !niday;
-     brightness_state++;
-     brightness_state = brightness_state % 3;
-
-
-   }
+//    Serial.println("Button pressed");
+    // Toggle LED state if button was pressed
+    brightness_state++;
+    brightness_state = brightness_state % 3;
+    if (brightness_state == 2){
+      FastLED.setBrightness(50);
+    }
+    else if (brightness_state == 0){
+      FastLED.setBrightness(0);
+    }
+    else if (brightness_state == 1){
+      FastLED.setBrightness(100);
+    }
+  }
 
   //measure period of tach signal
-//  ighigh = pulseIn(5,HIGH);
-//  iglow = pulseIn(5,LOW);
-//
-//  igcal1=1000/((ighigh/1000)+(iglow/1000));
-//
-//  //do it again
-//  ighigh = pulseIn(5,HIGH);
-//  iglow = pulseIn(5,LOW);
-//
-//  igcal2=1000/((ighigh/1000)+(iglow/1000));
-//  //to filter out some noise, we only consider our measurement valid if they are similar in value, we accept the average
-//  if((igcal1-igcal2)<8)igfreq = (igcal1+igcal2)/2;
-//
-//  //displaying measured freq through serial for debugging 
-//  Serial.print(cnt++);
-//  Serial.print("  Freq: ");
-//  Serial.println(igfreq);
-//  delay(1);
+  //  ighigh = pulseIn(5,HIGH);
+  //  iglow = pulseIn(5,LOW);
+  //
+  //  igcal1=1000/((ighigh/1000)+(iglow/1000));
+  //
+  //  //do it again
+  //  ighigh = pulseIn(5,HIGH);
+  //  iglow = pulseIn(5,LOW);
+  //
+  //  igcal2=1000/((ighigh/1000)+(iglow/1000));
+  //  //to filter out some noise, we only consider our measurement valid if they are similar in value, we accept the average
+  //  if((igcal1-igcal2)<8)igfreq = (igcal1+igcal2)/2;
+  //
+  //  //displaying measured freq through serial for debugging
+  //  Serial.print(cnt++);
+  //  Serial.print("  Freq: ");
+  //  Serial.println(igfreq);
+  //  delay(1);
 
 
   //day mode LED setup
   //Progressive lights from 3000-6500, 6 green lights, then yellow, then blue.
   // @6500 all flashing blue
-  // @7000 all flashing red 
+  // @7000 all flashing red
   igfreq = manual_igfreq;
 
-  int rpm = igfreq/30;
+  //  if(niday == HIGH){
+  if (brightness_state == 0) {
+    for (i = 0; (i < NUMPIXELS); i++) {
 
-  int red[3];
-  int green[3];
-  int blue[3];
-  int yellow[3];
-  
-  if (brightness_state == 1){
-    memcpy(red, _red, 3*sizeof(int));
-    memcpy(green, _green, 3*sizeof(int));
-    memcpy(blue, _blue, 3*sizeof(int));
-    memcpy(yellow, _yellow, 3*sizeof(int));
-  } 
-  else if (brightness_state == 2){
-    memcpy(red, _dimred, 3*sizeof(int));
-    memcpy(green, _dimgreen, 3*sizeof(int));
-    memcpy(blue, _dimblue, 3*sizeof(int));
-    memcpy(yellow, _dimyellow, 3*sizeof(int));
-  }
+      pixels[i] = CRGB::Black;
 
-  // If the current RPM is below the trigger RPM,
-  //  or brightness is set to OFF, turn the strip off
-  //  and proceed.
-  if (rpm < MIN_RPM || brightness_state == 0){
-
-    lightPixels(pixels, 0, NUMPIXELS, _off);
-  }
-
-  // Light green pixels
-  else if (rpm > MIN_RPM && rpm < SHIFT_RPM){
-
-    // Light the LEDs using the number of green pixels * ratio of current rpm to shift rpm
-    int litPixels = (NUMPIXELS) * (rpm - MIN_RPM)/(SHIFT_RPM-MIN_RPM);
-
-    //If it's only green pixels..
-    if (litPixels <= NUMPIXELS - YLWPIXELS){
-
-      lightPixels(pixels, 0, litPixels, green);
-    }
-
-    //If both green and yellow should be lit
-    else if (litPixels > (NUMPIXELS-YLWPIXELS)){
-
-      //    Pixels / RPM = NUMPIXELS / (shift - min)
-      //    Number of pixels to be lit is NUMPIXELS / (shift-min) * (rpm-min)
-      //    If this number is > total pixels - yellow pixels, light the green
-      //    otherwise also light the yellow
-
-      //Light the green pixels
-      lightPixels(pixels, 0, (NUMPIXELS-YLWPIXELS), green);
-
-      //Light the yellow pixels
-      lightPixels(pixels, (NUMPIXELS-YLWPIXELS)+1, litPixels, yellow);
     }
   }
 
-  //Flash blue to shift
-  else if (rpm >= SHIFT_RPM && rpm < MAX_RPM){
+  //As you approach the shift point, light up
+  if (igfreq >= 100 && igfreq < 216) {
+    for ( i = 0; (i < NUMPIXELS) && (i <= ((igfreq - 100) / 16)); i++) {
 
-    lightPixels(pixels, 0, NUMPIXELS, _blue);
-    delay(20);
-    pixels.show();
-    lightPixels(pixels, 0, NUMPIXELS, _off);
-    delay(20);
+      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+      if (i < 6) {
+        pixels[i] = CRGB::Green;
+      }
+      if (igfreq > 196) {
+        pixels[i] = CRGB::Yellow;
+      }
+      if (igfreq > 212) {
+        pixels[i] = CRGB::Blue;
+      }
+    }
+
+   //Useless code was here
   }
 
-  //Flash red when you're past max
-  else if (rpm >= MAX_RPM){
+  // If you're at the shift point, strobe blue
+  else if (igfreq >= 216 && igfreq < 230) {
+    for ( i = 0; i < NUMPIXELS; i++) {
+      
+      pixels[i] = CRGB::Blue;
+      FastLED.show();
+    }
+    
+    delay(10);
+    for (i = 0; i < NUMPIXELS; i++) {
 
-    lightPixels(pixels, 0, NUMPIXELS, _red);
-    delay(20);
-    pixels.show();
-    lightPixels(pixels, 0, NUMPIXELS, _off);
-    delay(20);
+      pixels[i] = CRGB::Black;
+      FastLED.show();
+    }
+    delay(10);
   }
 
-   pixels.show();
-}
+  // If above target RPM, strobe red
+  else if (igfreq >= 230) {
+    for ( i = 0; (i < NUMPIXELS); i++) {
 
-// Light a range of pixels with a given color
-void lightPixels(Adafruit_NeoPixel pixels, int pixelStart, int pixelEnd, int rgb[]){
+      pixels[i] = CRGB::Red;
+      FastLED.show();
+    }
+    delay(10);
+    for ( i = 0; (i < NUMPIXELS); i++) {
+      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
 
-  for(i=0;(i<(pixelEnd-pixelStart));i++){
-
-    lightPixel(pixels,pixelStart+i,rgb);
+      pixels[i] = CRGB::Black;
+      FastLED.show();
+    }
+    
+    delay(10);
   }
+
+  // Else, turn the strip off
+  else for (i = 0; (i < NUMPIXELS); i++) {
+
+      pixels[i] = CRGB::Black;
+    }
+    
+    FastLED.show();
 }
-
-// Light a single pixel
-void lightPixel(Adafruit_NeoPixel pixels, int pixelNum, int rgb[]){
-
-  int red = rgb[0];
-  int green = rgb[1];
-  int blue = rgb[2];  
-
-  pixels.setPixelColor(pixelNum, pixels.Color(red, green, blue));
-}
-
